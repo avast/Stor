fa86c253f14e2d3b0692ab39e0bc584fde426760
Miroslav Tynovsky <tynovsky@avast.com>
2017-03-16 15:44:56 +0100

    minilla scaffold

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Stor - It's new $module
+
+# SYNOPSIS
+
+    use Stor;
+
+# DESCRIPTION
+
+Stor is ...
+
+# LICENSE
+
+Copyright (C) Miroslav Tynovsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Miroslav Tynovsky <tynovsky@avast.com>
---
+++ lib/Stor.pm
@@ -0,0 +1,39 @@
+package Stor;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Stor - It's new $module
+
+=head1 SYNOPSIS
+
+    use Stor;
+
+=head1 DESCRIPTION
+
+Stor is ...
+
+=head1 LICENSE
+
+Copyright (C) Miroslav Tynovsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Miroslav Tynovsky E<lt>tynovsky@avast.comE<gt>
+
+=cut
+
